ff04e386f6e28cb8da4d76e28e1f0bc0a0ea045a
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-05-17 11:48:05 +0200

    FIAE-172: Added an app config

---
+++ planning_poker_jira/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class PlanningPokerJiraConfig(AppConfig):
+    name = 'planning_poker_jira'
+    verbose_name = _('Planning Poker Jira')
