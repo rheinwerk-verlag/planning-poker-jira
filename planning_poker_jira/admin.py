471f2810e10d49c1eb0c8c411b4a568a45c93838
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-06-10 12:47:28 +0200

    FIAE-172: Moved the forms into their own forms.py module

--- planning_poker_jira/admin.py
+++ planning_poker_jira/admin.py
@@ -1,7 +1,6 @@
 from requests.exceptions import ConnectionError, RequestException
 from typing import Dict, List, Union
 
-from django import forms
 from django.contrib import admin, messages
 from django.contrib.admin import helpers, ModelAdmin
 from django.contrib.admin.templatetags.admin_urls import admin_urlname
@@ -10,14 +9,13 @@ from django.db.models import QuerySet
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.template.response import TemplateResponse
 from django.urls import reverse, URLResolver, URLPattern
-from django.utils.functional import cached_property
 from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _, ngettext_lazy
-from jira import JIRA, JIRAError
+from jira import JIRAError
 
 from planning_poker.admin import StoryAdmin
-from planning_poker.models import PokerSession
 
+from .forms import ExportStoriesForm, ImportStoriesForm, JiraConnectionForm
 from .models import JiraConnection
 from .utils import get_jira_error_error_text
 
@@ -91,88 +89,6 @@ def export_stories(modeladmin: ModelAdmin, request: HttpRequest, queryset: Query
         'media': modeladmin.media
     }
     return TemplateResponse(request, 'admin/planning_poker/story/export_stories.html', context)
-
-
-class JiraAuthenticationForm(forms.Form):
-    username = forms.CharField(label=_('Username'),
-                               help_text=_('You can use this to override the username saved in the database'),
-                               required=False)
-    password = forms.CharField(label=_('Password'),
-                               help_text=_('You can use this to override the password in the database'),
-                               required=False,
-                               widget=forms.PasswordInput)
-
-    def __init__(self, *args, **kwargs):
-        self._connection = kwargs.pop('connection', None)
-        super().__init__(*args, **kwargs)
-
-    @cached_property
-    def client(self) -> JIRA:
-        if self.errors:
-            raise ValueError('Could not get the client because the data did not validate')
-        if self.connection:
-            client = self.connection.get_client()
-        else:
-            client = JIRA(self.cleaned_data.get('api_url'),
-                          basic_auth=(self.cleaned_data.get('username'), self.cleaned_data.get('password')))
-        return client
-
-    @cached_property
-    def connection(self) -> JiraConnection:
-        if self.errors:
-            raise ValueError('Could not get the connection because the data did not validate')
-        return self.cleaned_data.get('jira_connection') or self._connection or self.instance
-
-    def clean(self) -> dict:
-        cleaned_data = super().clean()
-
-        api_url = cleaned_data.get('api_url') or getattr(self.connection, 'api_url', None)
-        username = cleaned_data.get('username') or getattr(self.connection, 'username', None)
-        # We override `cleaned_data['password']` because it would otherwise reset the model's password to an empty
-        # string if the user didn't enter anything in the form's password field.
-        password = cleaned_data['password'] = cleaned_data.get('password') or getattr(self.connection, 'password', None)
-        if not (api_url and username):
-            self.add_error(None,
-                           _('Missing credentials. Check whether you entered an API URL, an username and a password'))
-        # We don't have to verify the credentials if the user hasn't provided a password.
-        if password:
-            try:
-                JIRA(api_url, basic_auth=(username, password))
-            except JIRAError as e:
-                self.add_error(None, get_jira_error_error_text(e))
-            except ConnectionError:
-                self.add_error(None, _('Failed to connect to server. Is "{}" the correct API URL?').format(api_url))
-            except RequestException:
-                self.add_error(None,
-                               _('There was an ambiguous error with your request. Check if all your data is correct.'))
-        return cleaned_data
-
-
-class JiraConnectionForm(JiraAuthenticationForm, forms.ModelForm):
-    username = forms.CharField(label=_('Username'),
-                               required=False)
-    password = forms.CharField(label=_('Password'),
-                               required=False,
-                               widget=forms.PasswordInput)
-
-
-class ImportStoriesForm(JiraAuthenticationForm):
-    poker_session = forms.ModelChoiceField(
-        label=_('Poker Session'),
-        help_text=_('The poker session to which the imported stories should be added'),
-        queryset=PokerSession.objects.all(),
-        required=False
-    )
-    jql_query = forms.CharField(label=_('JQL Query'), required=True)
-
-
-class ExportStoriesForm(JiraAuthenticationForm):
-    jira_connection = forms.ModelChoiceField(
-        label=_('Jira Connection'),
-        help_text=_('The Jira Backend to which the stories should be exported'),
-        queryset=JiraConnection.objects.all(),
-        required=True
-    )
 
 
 @admin.register(JiraConnection)
