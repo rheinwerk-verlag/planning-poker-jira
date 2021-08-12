5d17a4d6e03af55eb748db8721f141b3461665f2
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-08-12 08:57:01 +0200

    Override the app config for django channels presence.

--- planning_poker_jira/apps.py
+++ planning_poker_jira/apps.py
@@ -1,3 +1,4 @@
+from channels_presence.apps import RoomsConfig
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
@@ -5,3 +6,9 @@ from django.utils.translation import gettext_lazy as _
 class PlanningPokerJiraConfig(AppConfig):
     name = 'planning_poker_jira'
     verbose_name = _('Planning Poker: Jira Extension')
+
+
+class ChannelsPresenceConfig(RoomsConfig):
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'channels_presence'
+    verbose_name = _('Channels Presence')
