bdb28fee6442bfab6e6698441b3c8017650da4d2
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-08-12 14:37:30 +0200

    Planning-poker's `ChannelsPresenceConfig` is now used to override the django-channels-presence app config.

--- example/settings.py
+++ example/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.admindocs',
     'channels',
-    'planning_poker_jira.apps.ChannelsPresenceConfig',
+    'planning_poker.apps.ChannelsPresenceConfig',
     'planning_poker',
     'encrypted_fields',
     'planning_poker_jira',
--- planning_poker_jira/apps.py
+++ planning_poker_jira/apps.py
@@ -1,4 +1,3 @@
-from channels_presence.apps import RoomsConfig
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
@@ -8,9 +7,3 @@ class PlanningPokerJiraConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'planning_poker_jira'
     verbose_name = _('Planning Poker: Jira Extension')
-
-
-class ChannelsPresenceConfig(RoomsConfig):
-    default_auto_field = 'django.db.models.AutoField'
-    name = 'channels_presence'
-    verbose_name = _('Channels Presence')
