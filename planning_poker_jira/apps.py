de1ffc63e4834c6c087f3d2ab25debe880418de1
Thomas Scheller <thomas.scheller@rheinwerk-verlag.de>
2021-08-12 09:18:21 +0200

    Fixed Django 3.2 warnings.

--- planning_poker_jira/apps.py
+++ planning_poker_jira/apps.py
@@ -4,6 +4,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class PlanningPokerJiraConfig(AppConfig):
+    default = True
+    default_auto_field = 'django.db.models.AutoField'
     name = 'planning_poker_jira'
     verbose_name = _('Planning Poker: Jira Extension')
 
